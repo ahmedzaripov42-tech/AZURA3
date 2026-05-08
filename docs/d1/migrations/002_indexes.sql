a0a9eacdf6f5b2d3bdb73c1381d681522b1f8192
nikarobot009-hub <nikarobot009-hub@users.noreply.github.com>
2026-05-08 19:55:54 +0500

    Replace site with Worker API

--- docs/d1/migrations/002_indexes.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX idx_manga_status ON manga(status);
-CREATE INDEX idx_manga_views ON manga(views);
-CREATE INDEX idx_chapters_manga_id ON chapters(manga_id);
-CREATE INDEX idx_comments_manga_id ON comments(manga_id);
-CREATE INDEX idx_users_email ON users(email);
